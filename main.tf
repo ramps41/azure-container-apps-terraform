ec0086b44439fb46360a5e2169e1d6129dc7415f
Andrew Rampling <5417588+ramps41@users.noreply.github.com>
2025-12-23 16:30:17 +1030

    ca env

--- main.tf
+++ main.tf
@@ -11,3 +11,11 @@ resource "azurerm_log_analytics_workspace" "law" {
   sku               = "PerGB2018"
   retention_in_days = 30
 }
+
+resource "azurerm_container_app_environment" "cae" {
+  name                = "${local.container_app_name_prefix}-cae"
+  location            = azurerm_resource_group.container_app_rg.location
+  resource_group_name = azurerm_resource_group.container_app_rg.name
+
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+}
